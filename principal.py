6dc646bde77b41eb695cef2c170c7a5e19967eec
Jhonatas Matos <jhonatas.matos@yaman.com.br>
2019-06-26 21:33:15 -0300

    adicionado os testes nos metodos de multiplicação e divisão

--- principal.py
+++ principal.py
@@ -5,3 +5,9 @@ def somar(x, y):
 
 def sub(x, y):
     return x - y
+
+def multi(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
--- teste.py
+++ teste.py
@@ -1,9 +1,17 @@
 import pytest
 from principal import somar
 from principal import sub
+from principal import multi
+from principal import div
 
 def test_somar():
     assert somar(2,3)==5
 
 def test_sub():
     assert sub(5,2)==3
+
+def test_multiplicar():
+    assert somar(2,3)==6
+
+def test_dividir():
+    assert sub(10,2)==5
